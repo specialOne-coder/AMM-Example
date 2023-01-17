7c65b93ddbec0d1c1f680829c04933c078075813
ferdinand <ferdinand@kalinan.kalinan/subs>
2023-01-17 22:45:25 +0100

    smart contract

--- contracts/oracles/Oracle.sol
+++ contracts/oracles/Oracle.sol
@@ -2,10 +2,16 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract PriceConsumerV3 {
+
+contract Bets is ChainlinkClient, ConfirmedOwner{
     AggregatorV3Interface internal priceFeed;
     uint256 betIds = 0;
+
+    bytes32 private jobId;
+    uint256 private fee;
 
     struct Bet {
         uint256 betAmount;
@@ -28,10 +34,11 @@ contract PriceConsumerV3 {
      * Aggregator: ETH/USD
      * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
      */
-    constructor() {
-        priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
+    constructor() ConfirmedOwner(msg.sender){
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
+        jobId = "ca98366cc7314957b8c012c72f05aeeb";
+        fee = (1 * LINK_DIVISIBILITY) 
     }
 
     // increment
@@ -71,9 +78,52 @@ contract PriceConsumerV3 {
         return price;
     }
 
-    function getHistoricalPrice(uint80 roundId) public view returns (int256) {
-        (, int price, , , ) = priceFeed.getRoundData(roundId);
-        return price;
+     function timestampToDate(uint timestamp) public view returns (string memory) {
+        uint year = timestamp / (365 * 24 * 60 * 60);
+        uint month = timestamp % (365 * 24 * 60 * 60) / (30 * 24 * 60 * 60);
+        uint day = timestamp % (30 * 24 * 60 * 60) / (24 * 60 * 60);
+        return format("%02d-%02d-%04d",day,month,year);
+    }
+
+     function requestVolumeData(uint timestamp) public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
+        
+
+        // Set the URL to perform the GET request on
+        req.add(
+            "get",
+            "https://api.coingecko.com/api/v3/coins/ethereum/history?date=" + timestamp
+        );
+
+        // Set the path to find the desired data in the API response, where the response format is:
+        // {"RAW":
+        //   {"ETH":
+        //    {"USD":
+        //     {
+        //      "VOLUME24HOUR": xxx.xxx,
+        //     }
+        //    }
+        //   }
+        //  }
+        // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
+        req.add("path", "market_data,current_price,ust"); // Chainlink nodes 1.0.0 and later support this format
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10 ** 18;
+        req.addInt("times", timesAmount);
+
+        // Sends the request
+        return sendChainlinkRequest(req, fee);
+    }
+
+    function getHistoricalPrice(uint80 roundId) public view returns (int256,uint) {
+        (, int price, ,uint timestamp , ) = priceFeed.getRoundData(roundId);
+        require(timestamp > 0, "Round not complete");
+        return (price,timestamp);
     }
 
     // bet on token price
